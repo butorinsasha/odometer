86df2ef2e0a2face15cb42a46c29f710abc0aa8f
pushkinaleksandr <butorinsasha@gmail.com>
2023-12-22 03:34:43 +0300

    Odometer finished

--- app/src/main/java/local/pushkin/odometer/OdometerService.java
+++ app/src/main/java/local/pushkin/odometer/OdometerService.java
@@ -1,15 +1,18 @@
 package local.pushkin.odometer;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 
 public class OdometerService extends Service {
     private final IBinder binder = new OdometerBinder();
@@ -27,7 +30,6 @@ public class OdometerService extends Service {
         return binder;
     }
 
-    @SuppressLint("MissingPermission")
     @Override
     public void onCreate() {
         LocationListener listener = new LocationListener() {
@@ -41,6 +43,16 @@ public class OdometerService extends Service {
             }
         };
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listener);
     }
 
@@ -48,4 +60,7 @@ public class OdometerService extends Service {
         return distanceInMeters / 1609.344;
     }
 
+    public double getDistanceInMeters() {
+        return distanceInMeters;
+    }
 }
